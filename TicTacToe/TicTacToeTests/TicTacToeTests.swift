7b3e8624422d89741abd910298184619db9f0fbd
vsemenyug <vsemenyug@gmail.com>
2025-01-12 21:18:00 +0100

    TicTacToe project implemented

---
+++ TicTacToe/TicTacToeTests/TicTacToeTests.swift
@@ -0,0 +1,17 @@
+//
+//  TicTacToeTests.swift
+//  TicTacToeTests
+//
+//  Created by Volodymyr Semenyug on 12.01.2025.
+//
+
+import Testing
+@testable import TicTacToe
+
+struct TicTacToeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
